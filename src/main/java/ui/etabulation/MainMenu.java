353128dbce39bd27f037f6c97f0c52708fe0ed69
MayNevVal <97944690+mnv121@users.noreply.github.com>
2025-05-16 15:33:55 +0800

    initial main menu for festival

--- src/main/java/ui/etabulation/MainMenu.java
+++ src/main/java/ui/etabulation/MainMenu.java
@@ -1,68 +1,66 @@
-package ui.etabulation;
-
-import java.net.URL;
-import javafx.application.Application;
-import static javafx.application.Application.launch;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
-import javafx.scene.Parent;
-import javafx.scene.image.Image;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import org.guanzon.appdriver.base.GRider;
-
-/**
- *
- * @author user
- */
-public class MainMenu extends Application {
-
-    public final static String pxeMainFormTitle = "E - Tabulation";
-    public final static String pxeMainForm = "/views/MainMenu.fxml";
-    public final static String pxeStageIcon = "org/rmj/etabulation/images/icon.png";
-    public static GRider oApp;
-
-    @Override
-    public void start(Stage primaryStage) {
-        try {
-            FXMLLoader view = new FXMLLoader();
-            view.setLocation(getClass().getResource(pxeMainForm));
-
-            MainMenuController controller = new MainMenuController();
-            controller.setGRider(oApp);
-
-            view.setController(controller);
-            Parent parent = view.load();
-            Scene scene = new Scene(parent);
-
-            //get the screen size
-            Screen screen = Screen.getPrimary();
-            Rectangle2D bounds = screen.getVisualBounds();
-
-            primaryStage.setScene(scene);
-            primaryStage.initStyle(StageStyle.UNDECORATED);
-            primaryStage.getIcons().add(new Image(pxeStageIcon));
-            primaryStage.setTitle(pxeMainFormTitle);
-
-            // set stage as maximized but not full screen
-            primaryStage.setX(bounds.getMinX());
-            primaryStage.setY(bounds.getMinY());
-            primaryStage.setWidth(bounds.getWidth());
-            primaryStage.setHeight(bounds.getHeight());
-            primaryStage.centerOnScreen();
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    public void setGRider(GRider foValue) {
-        oApp = foValue;
-    }
-}
+//package ui.etabulation;
+//
+//import java.net.URL;
+//import javafx.application.Application;
+//import static javafx.application.Application.launch;
+//import javafx.fxml.FXMLLoader;
+//import javafx.geometry.Rectangle2D;
+//import javafx.scene.Scene;
+//import javafx.scene.Parent;
+//import javafx.scene.image.Image;
+//import javafx.stage.Screen;
+//import javafx.stage.Stage;
+//import javafx.stage.StageStyle;
+//import org.guanzon.appdriver.base.GRider;
+//
+///**
+// *
+// * @author user
+// */
+//public class MainMenu extends Application {
+//
+//    public final static String pxeMainFormTitle = "E - Tabulation";
+//    public final static String pxeMainForm = "/views/MainMenu.fxml";
+//    public static GRider oApp;
+//
+//    @Override
+//    public void start(Stage primaryStage) {
+//        try {
+//            FXMLLoader view = new FXMLLoader();
+//            view.setLocation(getClass().getResource(pxeMainForm));
+//
+//            MainMenuController controller = new MainMenuController();
+//            controller.setGRider(oApp);
+//
+//            view.setController(controller);
+//            Parent parent = view.load();
+//            Scene scene = new Scene(parent);
+//
+//            //get the screen size
+//            Screen screen = Screen.getPrimary();
+//            Rectangle2D bounds = screen.getVisualBounds();
+//
+//            primaryStage.setScene(scene);
+//            primaryStage.initStyle(StageStyle.UNDECORATED);
+//            primaryStage.setTitle(pxeMainFormTitle);
+//
+//            // set stage as maximized but not full screen
+//            primaryStage.setX(bounds.getMinX());
+//            primaryStage.setY(bounds.getMinY());
+//            primaryStage.setWidth(bounds.getWidth());
+//            primaryStage.setHeight(bounds.getHeight());
+//            primaryStage.centerOnScreen();
+//            primaryStage.show();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        launch(args);
+//    }
+//
+//    public void setGRider(GRider foValue) {
+//        oApp = foValue;
+//    }
+//}
--- src/main/java/ui/etabulation/MainMenuController.java
+++ src/main/java/ui/etabulation/MainMenuController.java
@@ -2,43 +2,59 @@ package ui.etabulation;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Random;
 import java.util.ResourceBundle;
+import javafx.animation.Animation;
+import javafx.animation.TranslateTransition;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+import org.guanzon.appdriver.base.GRider;
 
 public class MainMenuController implements Initializable {
-    @FXML private MenuItem etabulation, about, closeApp;
-    @FXML private ImageView imgBanner;
-    @FXML private VBox BannerBoxV;
+
+    public static GRider oApp;
+
+    @FXML
+    private MenuItem mnuAbout, mnuScoring, mnuClose;
+    @FXML
+    private Pane pnMainCenter;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        imgBanner.setImage(new Image(getClass().getResourceAsStream("/images/BannerSample.png")));
-        imgBanner.fitWidthProperty().bind(BannerBoxV.widthProperty());
+        //to do list
+        Platform.runLater(() -> launchConfetti(pnMainCenter));
+
     }
 
     @FXML
     private void handleMenu(ActionEvent event) {
         MenuItem src = (MenuItem) event.getSource();
         switch (src.getId()) {
-            case "etabulation":
+            case "mnuScoring":
                 openWindow("/views/frmETabulation.fxml", "Scoring Module");
                 break;
-            case "about":
+            case "mnuAbout":
                 openWindow("/views/About.fxml", "About This App");
                 break;
-            case "closeApp":
+            case "mnuClose":
                 Platform.exit();
                 break;
             default:
@@ -48,13 +64,59 @@ public class MainMenuController implements Initializable {
 
     private void openWindow(String fxmlPath, String title) {
         try {
+            
+            //get the screen size
+            Screen screen = Screen.getPrimary();
+            Rectangle2D bounds = screen.getVisualBounds();
             Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
+            
             Stage stage = new Stage();
+             // set stage as maximized but not full screen
+            stage.setX(bounds.getMinX());
+            stage.setY(bounds.getMinY());
+            stage.setWidth(bounds.getWidth());
+            stage.setHeight(bounds.getHeight());
+            
             stage.setTitle(title);
+            stage.initStyle(StageStyle.UNDECORATED);
             stage.setScene(new Scene(root));
+            stage.centerOnScreen();
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    void setGRider(GRider poApp) {
+        oApp = poApp;
+    }
+
+    public void launchConfetti(Pane root) {
+        Random random = new Random();
+        int width = (int) (root.getWidth() > 0 ? root.getWidth() : 400);
+        int height = (int) (root.getHeight() > 0 ? root.getHeight() : 900);
+
+        for (int i = 0; i < 30; i++) {
+            Circle confetti = new Circle(4);
+            confetti.setFill(Paint.valueOf(randomColor()));
+            confetti.setLayoutX(random.nextInt(width));
+            confetti.setLayoutY(10);
+
+            TranslateTransition fall = new TranslateTransition(Duration.seconds(5 + random.nextInt(3)), confetti);
+            fall.setToY(height + 30);
+            fall.setCycleCount(Animation.INDEFINITE);
+            fall.setDelay(Duration.seconds(random.nextDouble() * 5));
+            fall.play();
+
+            root.getChildren().add(confetti);
+        }
+
+    }
+
+    private String randomColor() {
+        String[] neonColors = {
+            "#facc15", "#00ffd0", "#ff4fd8", "#00bfff", "#ffbf00"
+        };
+        return neonColors[new Random().nextInt(neonColors.length)];
     }
 }
